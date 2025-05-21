3dfe79aba1518f5ebf9eb0e3d81b51caa14ffed5
Keased <59649582+Keased@users.noreply.github.com>
2025-05-21 13:06:29 +0300

    Update vite.config.js with proxy

--- vite.config.js
+++ vite.config.js
@@ -2,5 +2,15 @@ import {defineConfig} from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-    plugins: [react()],
+  plugins: [react()],
+  server: {
+    proxy: {
+      '/external': {
+        target: 'https://youmentorme-m6lzj4bpgzcgmgw8.builder-preview.com/',
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/external/, ''),
+      },
+    },
+  },
 });
+
